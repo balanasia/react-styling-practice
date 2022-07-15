2d159542259e7b19b8a66c24ff1c1335cbd3f091
Anastasiia <41988936+balanasia@users.noreply.github.com>
2022-07-15 15:01:57 -0500

    attempt to do the JSX styling challenge

--- src/index.js
+++ src/index.js
@@ -5,3 +5,32 @@
 //Apply the "heading" style in the styles.css
 //Dynamically change the color of the h1 using inline css styles.
 //Morning = red, Afternoon = green, Night = blue.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const d = new Date();
+const customStyle = {
+  color: "red"
+};
+
+let h1Text = "Good morning!";
+
+console.log(d.getHours());
+
+if (d.getHours() >= 12 || d.getHours() < 18) {
+  h1Text = "Good Afternoon!";
+  customStyle.color = "green";
+} else if (d.getHours() >= 18 || d.getHours() < 24) {
+  h1Text = "Good Evening";
+  customStyle.color = "blue";
+}
+
+console.log(customStyle);
+
+ReactDOM.render(
+  <div>
+    <h1 style={customStyle}>{h1Text}</h1>
+  </div>,
+  document.getElementById("root")
+);
